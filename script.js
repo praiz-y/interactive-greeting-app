ebe1133d90e348f6f753c79fc41318cb208c2c2a
Variant <praisekaye13@gmail.com>
2026-02-23 05:57:43 +0100

    Add reset button and enter key support

--- script.js
+++ script.js
@@ -1,21 +1,41 @@
-
-
-  
-// Wait for the DOM to fully load
-document.addEventListener('DOMContentLoaded', function() {
-    // Get references to the input, button, and greeting message elements
-    const nameInput = document.getElementById('nameInput');
+document.addEventListener('DOMContentLoaded', () => {
+    const nameInput = document.getElementById('nameInput'); 
     const greetButton = document.getElementById('greetButton');
     const greetingMessage = document.getElementById('greetingMessage');
-    // Add a click event listener to the greet button
-    greetButton.addEventListener('click', function() {
-        // Get the value from the input field
+    const resetButton  = document.getElementById('resetButton');
+
+    const handleGreeting = () =>{
         const name = nameInput.value.trim();
-        // Check if the name is not empty
+        const greeting = getTime();
         if (name) {
-            greetingMessage.textContent = `Hello, ${name}! Welcome to the Greeting App!`;
-        } else {
-            // If the input is empty, prompt the user to enter their name
-            greetingMessage.textContent = 'Please enter your name to receive a greeting.';
-        }  });
+            
+            greetingMessage.textContent = `${greeting} ${name}, Welcome here`;
+        }else{
+            window.alert("Please insert a name")
+        }
+
+    };
+
+    greetButton.addEventListener('click', handleGreeting);
+    nameInput.addEventListener('keydown', (event) => {
+        if (event.key === 'Enter') {
+            handleGreeting();
+        }
+    });
+    resetButton.addEventListener('click', () => {
+        greetingMessage.textContent = "";
+        nameInput.value = "";
+        nameInput.focus();
+    });
 });
+
+
+
+
+const getTime = () => {
+    const hour = new Date().getHours();
+    
+            if (hour < 12) return "Good Morning oo";
+            if (hour < 18) return "Good Afternoon";
+            return "Good Evening";
+};
